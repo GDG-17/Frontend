d3f31263df0274bfdacdc1ac5b05761876c75e9b
me1e <mele0404@naver.com>
2022-06-26 02:41:25 +0900

    feat: widget

--- src/router.ts
+++ src/router.ts
@@ -2,31 +2,36 @@
 import { createWebHistory, createRouter } from "vue-router";
 
 const routes = [
-    {
-        path: "/",
-        name: "Home",
-        component: () => import("./pages/Home.vue"),
-    },
-    {
-        path: "/add-friend",
-        name: "AddFriend",
-        component: () => import("./pages/AddFriend.vue"),
-    },
-    {
-        path: "/notice",
-        name: "Notice",
-        component: () => import("./pages/Notice.vue"),
-    },
-    {
-        path: "/change-status",
-        name: "ChangeStatus",
-        component: () => import("./pages/ChangeStatus.vue"),
-    },
+  {
+    path: "/",
+    name: "Home",
+    component: () => import("./pages/Home.vue"),
+  },
+  {
+    path: "/add-friend",
+    name: "AddFriend",
+    component: () => import("./pages/AddFriend.vue"),
+  },
+  {
+    path: "/notice",
+    name: "Notice",
+    component: () => import("./pages/Notice.vue"),
+  },
+  {
+    path: "/change-status",
+    name: "ChangeStatus",
+    component: () => import("./pages/ChangeStatus.vue"),
+  },
+  {
+    path: "/widget",
+    name: "Widget",
+    component: () => import("./pages/Widget.vue"),
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
